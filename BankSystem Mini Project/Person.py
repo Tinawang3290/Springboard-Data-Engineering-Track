90168926179a77f60053376055e2b03141c12811
Tina Wang <37784402+Tinawang3290@users.noreply.github.com>
2021-04-25 21:40:52 -0700

    Update Person.py

--- BankSystem Mini Project/Person.py
+++ BankSystem Mini Project/Person.py
@@ -1,6 +1,7 @@
 import pickle
 from Account import CheckingAccount, SavingsAccount
 
+#1 create Person Parent Class
 class Person:
     def __init__(self, first_name, last_name, addr, phone_num):   
         self.first_name = first_name
@@ -8,7 +9,7 @@ class Person:
         self.address = addr
         self.phone_num = phone_num
         self.profile_dict ={   #initiate the dictionary here for storage later
-            'name': self.first_name + self.last_name,
+            'name': self.first_name + " "+self.last_name,
             'address': self.address,
             'phone': self.phone_num
         }
@@ -18,16 +19,16 @@ class Person:
         '''
         To print out the person object's profile information.
         '''
-        print('User Name: {} {} \nAddress: {} \nPhone Number: {}'.format(self.first_name, self.last_name, self.address, self.phone_num))
-     
-# create child classes of the parent class: Person that inherits all the properties and methods from it.
+        print('User Name: {} {} \nAddress: {} \nPhone Number: {}'.format(self.first_name, self.last_name, self.address, self.phone_num))   
 
-class Customer(Person):
+class Customer(Person): 
+    '''
+    Customer is a child class of Person parent class that can inherits everything from Person's class.
+    '''
     def __init__(self, first_name, last_name, addr, phone_num):
         Person.__init__(self, first_name, last_name, addr, phone_num)
         self.accounts_available = {}  # key: account_num and values: account obj.
         self.services_available = {}  # key: service_num and values: service obj.
-        #
         self.profile_dict['accounts_available'] = self.accounts_available
         self.profile_dict['services_available'] = self.services_available
         
@@ -39,17 +40,16 @@ class Customer(Person):
         while True:
             create_account_input = input('Please type "S" to create SavingsAccount and "C" to create CheckingsAccount, else to exit: ')
             if create_account_input == 'C':
-                account_type = CheckingsAccount()
-                print('Welcome to the Account System! Your CheckingsAccount {} has been created!'.format(account_type.account_num))
-                self.accounts_available[account_type.account_num] = account_type
+                account_obj = CheckingsAccount()
+                print('Welcome to the Account System! Your CheckingsAccount {} with account balance of ${} has been created!'.format(account_obj.account_num, account_obj.balance_amount))
+                self.accounts_available[account_obj.account_num] = account_obj
                 continue
             elif create_account_input == 'S':
-                account_type = SavingsAccount()
-                print('Welcome to the Account System! Your SavingsAccount {} has been created!'.format(account_type.account_num)) # indicates the SavingsAccount has been created successfully.    
-                self.accounts_available[account_type.account_num] = account_type
+                account_obj= SavingsAccount()
+                print('Welcome to the Account System! Your SavingsAccount {} with account balance of ${} has been created!'.format(account_obj.account_num, account_obj.balance_amount)) # indicates the SavingsAccount has been created successfully.    
+                self.accounts_available[account_obj.account_num] = account_obj
                 continue
             else:
-                print('You are all set!')  
                 break 
         with open('accounts_available.pkl','wb') as accounts_file:         # using pickle to store accounts  
             pickle.dump(self.profile_dict, accounts_file)
@@ -67,26 +67,22 @@ class Customer(Person):
         while True:
             create_service_input = input('Please type "C" to create CreditCard and "L" to create Loan, else to eixt: ')
             if create_service_input == 'C':
-                service_type = CreditCard(self, first_name, last_name, salary, credit_limit=10000)
-                print('Welcome to the Service System! Your CreditCard {} has been created!'.format(service_type.service_num))
-                self.services_available[service_type.service_num] = service_type
+                service_obj = CreditCard()
+                service_obj.make_purchase() # trigger the make_purchase method from CreditCard class so that we don't have to create a separate creditcard object for testing.
+                self.services_available[service_obj.service_num] = service_obj
                 continue
             elif create_service_input == 'L':
-                service_type = Loan(self, first_name, last_name, int_rate = 0.35, loan_amount = 100000)
-                print('Welcome to the Service System! Your Loan {} has been created!'.format(service_type.service_num))     
-                self.accounts_available[service_type.account_num] = service_type
+                service_obj = Loan()   
+                self.services_available[service_obj.service_num] = service_obj
                 continue
             else:
-                print('You are all set!')  
                 break 
-        with open('services_available.pkl','wb') as serivces_file:         # using pickle to store services  
+        with open('services_available.pkl','wb') as services_file:         # using pickle to store services  
             pickle.dump(self.profile_dict, services_file)
         with open('services_available.pkl','rb') as services_file:
             display_services = pickle.load(services_file)
             print(display_services) 
-        print('the account is {}'.format(self.services_available.values()))
-        print('Your current available accounts are {}.'.format(self.services_available.keys())) 
-
+        print('Your current available services are {}.'.format(self.services_available.keys())) 
         print('----------------------------------------------------')
         return self.services_available
 
@@ -120,7 +116,7 @@ class Customer(Person):
         while True:
             transfer_from = input('Please select the account to transfer moeny from: ')
             transfer_to = input('Please select the account to transfer money to: ')
-            transfer_amount = input('Please enter amount to transfer in $: ')
+            transfer_amount = float(input('Please enter amount to transfer in $: '))
             self.transfer_from = transfer_from
             self.transfer_to = transfer_to
             self.transfer_amount = transfer_amount
@@ -145,27 +141,22 @@ class Customer(Person):
         while True:
             pay_from = input('Please select the account num to pay from: ')
             pay_to = input('Please select the service num to pay to: ')
-            payment_amount = input('Please enter payment amount in $: ')
+            payment_amount = float(input('Please enter payment amount in $: '))
             self.payment_amount = payment_amount
             self.pay_from = pay_from 
             self.pay_to = pay_to
-            if pay_from in self.profile_dict[accounts_available].keys() and pay_to in self.profile_dict[services_available].keys():
+            if pay_from in self.accounts_available.keys() and pay_to in self.services_available.keys():
                 self.accounts_available[self.pay_from].withdraw(self.payment_amount)
                 self.services_available[self.pay_to].receive_payment(self.payment_amount) 
                 print('${} has been made from account {} to service {}!'.format(self.payment_amount, self.pay_from, self.pay_to))
             else:
                 print('The account/service does not exist. Please try again!')        
-            more_input = input('Please enter "Y" to make another payment, elst to exit: ')
-            if more_input == 'Y':
-                continue
-            else:
-                break
-        print('You are all set!')  
+                more_input = input('Please enter "Y" to make another payment, elst to exit: ')
+                if more_input == 'Y':
+                    continue
+                else:
+                    break
         print('----------------------------------------------------')
-        self.service_balance -= self.payment_amount  # service_balance is basically accumulated debts from various services
-       # print('Your Total Service Balance is ${}'.format(self.service_balance))
-        print('----------------------------------------------------')
-        return self.service_balance
     
     def display_profile(self):
         '''
@@ -176,11 +167,13 @@ class Customer(Person):
         Person.display_profile(self)
         for acc in self.accounts_available.values():
             total_account_amount += acc.balance_amount
+            print('Account {} has a balance amount of ${}'.format(acc.account_num, acc.balance_amount))
         for ser in self.services_available.values():
             total_service_amount += ser.service_balance
-        print('Available Accounts: {} \nAccount Balance: {} \nAvailable Services: {} \nService Balance: {}'.format(self.accounts_available, total_account_amount, self.services_available, total_service_amount))
+            print('Service {} has a service balance amount of ${}'.format(ser.service_num, ser.service_balance))
+        print('Available Accounts: {} \nTotal Account Balance: {} \nAvailable Services: {} \nTotal Service Balance: {}'.format(self.accounts_available, total_account_amount, self.services_available, total_service_amount))
 
-class Employee(Customer):
+class Employee(Customer): # Employee is a child class of Customer class since it can inherit from Customer class if employee is also a customer.
     def __init__(self, first_name, last_name, addr, phone_num, salary):
         Customer.__init__(self, first_name, last_name, addr, phone_num) #inherit from parent class Customer
         self.salary = salary   
@@ -192,4 +185,4 @@ class Employee(Customer):
     def display_profile(self):
         Customer.display_profile(self)
         print('Current Annual Salary: ${} '.format(self.salary))
-
+        print('----------------------------------------------------')
