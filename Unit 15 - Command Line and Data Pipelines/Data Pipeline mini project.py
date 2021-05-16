8d22421a51361b3410cc8db5414ac8835a9bd6d3
Tina Wang <37784402+Tinawang3290@users.noreply.github.com>
2021-05-16 00:11:27 -0700

    Create Data Pipeline mini project.py

---
+++ Unit 15 - Command Line and Data Pipelines/Data Pipeline mini project.py
@@ -0,0 +1,91 @@
+import mysql.connector
+import pandas as pd
+
+
+def get_db_connection() -> object:
+    """
+    Connect to mysql database using credentials
+    :rtype: object
+    """
+    connection = None
+    try:
+        connection = mysql.connector.connect(user='root',
+                                             password='Kyleling1220@',
+                                             host='localhost',
+                                             port='3306',
+                                             database='ticket_system')
+    except Exception as error:
+        print("Error while connecting to database for job tracker", error)
+    return connection
+
+
+def load_third_party(conn, file_path_csv):
+    """
+    - Create the database and table
+    - Use DataFrame to read CSV source file and insert rows into created table
+    :param conn:
+    :param file_path_csv:
+    :return:
+    """
+    try:
+        cursor = conn.cursor()
+    # [Iterate through the CSV file and execute insert statement]
+        sql_ddl_statement = """
+        DROP DATABASE IF EXISTS ticket_system;
+        CREATE DATABASE ticket_system;
+        USE ticket_system;
+        CREATE TABLE ticket_sales(
+            ticket_id INT,
+            trans_date DATE,
+            event_id INT,
+            event_name VARCHAR(50),
+            event_date DATE,
+            event_type VARCHAR(10),
+            event_city VARCHAR(20),
+            customer_id INT,
+            price DECIMAL(7,2),
+            num_tickets INT,
+            PRIMARY KEY(ticket_id) --> To ensure no duplicate entries will be inserted.
+            );
+        """
+        source_data = pd.read_csv(file_path_csv, header=None)
+        cursor.execute(sql_ddl_statement)
+        # cursor.execute('SHOW DATABASES')
+        print('Database and table are created.')
+        for i, row in source_data.iterrows():
+            insert_data = """INSERT INTO ticket_system.ticket_sales
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"""
+            # print(row)
+            cursor.execute(insert_data, tuple(row))
+        conn.commit()
+        cursor.close()
+    except Exception as e:
+        cursor.close()
+        print("Error while connecting to MySQL", e)
+
+
+def query_popular_tickets(conn):
+    """
+    - Get the most popular ticket in the past month
+    :param conn:
+    :return:
+    """
+    sql_statement = '''SELECT event_name, SUM(num_tickets) as total_tickets FROM ticket_system.ticket_sales
+    GROUP BY event_name 
+    ORDER BY SUM(num_tickets) DESC 
+    LIMIT 1
+    '''
+    cursor = conn.cursor()
+    cursor.execute(sql_statement)
+    records = cursor.fetchall()
+    cursor.close()
+    return records
+
+
+if __name__ == '__main__':
+    conn = get_db_connection()
+    file_path_csv = '~/downloads/third_party_sales_1.csv'
+    load_third_party(conn,file_path_csv)
+
+
+
