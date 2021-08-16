8d1ed39da1e41183ea2e5941d14e697751a97bf8
Tina Wang <37784402+Tinawang3290@users.noreply.github.com>
2021-08-15 18:18:24 -0700

    Update and rename bash_script.sh.py to mapreduce1.sh
    hadoop jar /usr/local/Cellar/hadoop/3.3.1/libexec/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
    -file /Users/tinawang/PycharmProjects/pythonProject/Hadoop_Mapreduce_Mini_Project/Mapper1.py -mapper 'python3 Mapper1.py' \
    -file  /Users/tinawang/PycharmProjects/pythonProject/Hadoop_Mapreduce_Mini_Project/Reducer1.py -reducer 'python3 Reducer1.py' \
    -input /hadoop_project_folder/Hadoop_Mapreduce_Mini_Project/data.csv -output /hadoop_project_folder/output1

--- Hadoop Mini Project/bash_script.sh.py
+++
@@ -1,9 +0,0 @@
-hadoop jar /usr/local/Cellar/hadoop/3.3.1/libexec/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
--file /Users/tinawang/PycharmProjects/pythonProject/Hadoop_Mapreduce_Mini_Project/Mapper1.py -mapper 'python3 Mapper1.py' \
--file  /Users/tinawang/PycharmProjects/pythonProject/Hadoop_Mapreduce_Mini_Project/Reducer1.py -reducer 'python3 Reducer1.py' \
--input /hadoop_project_folder/data.csv -output /hadoop_project_folder/output1
-
-hadoop jar /usr/local/Cellar/hadoop/3.3.1/libexec/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
--file /Users/tinawang/PycharmProjects/pythonProject/Hadoop_Mapreduce_Mini_Project/Mapper2.py -mapper 'python3 Mapper2.py' \
--file  /Users/tinawang/PycharmProjects/pythonProject/Hadoop_Mapreduce_Mini_Project/Reducer2.py -reducer 'python3 Reducer2.py' \
--input /hadoop_project_folder/output1 -output /hadoop_project_folder/output2
